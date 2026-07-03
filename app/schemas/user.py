b5ca69e8c3561bb763779d4fe7888820284b99c2
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-03 18:14:37 +0300

    добавил схемы пользователя

---
+++ app/schemas/user.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserCreateSchema(BaseModel):
+    username: str = Field(
+        min_length=3,
+        max_length=50,
+        )
+    email: EmailStr
+    bio: str | None = None
+
+class UserResponseSchema(BaseModel):
+    id: str
+    username: str
+    email: EmailStr
+    bio: str | None = None
+    created_at: datetime
+
+class UserUpdateSchema(BaseModel):
+    username: str | None = Field(None, min_length=3, max_length=50)
+    bio: str | None = None
