4598e367dfeffe64fa1ffebfe6d80c6c6d437e1a
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-07 22:37:47 +0300

    сделаны схемы для статей

---
+++ app/schemas/article.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+from .user import UserResponseSchema
+
+class ArticleCreateSchema(BaseModel):
+    title: str = Field(min_length=1, max_length=200)
+    content: str = Field(min_length=1)
+    author_id: str
+
+class ArticleUpdateSchema(BaseModel):
+    title: str | None = None
+    content: str | None = None
+
+class ArticleResponseSchema(BaseModel):
+    id: str
+    title: str
+    content: str
+    author_id: str
+    created_at: datetime
+    updated_at: datetime
+
+class ArticleWithAuthorSchema(ArticleResponseSchema):
+    author: UserResponseSchema
+    
