174202d9e68d71687471444169dc08f9d2ad81c7
Sabridge22 <Alegzandr2004@gmail.com>
2026-07-09 23:53:16 +0300

    сделана функция изменения статьи в сервисе

--- app/services/article.py
+++ app/services/article.py
@@ -8,6 +8,9 @@ class ArticleNotFound(Exception):
     """статья не найдена"""
     ...
 
+class PermissionDenied(Exception):
+    """нет прав на это действие"""
+    pass
 
 class ArticleService:
     def __init__(self, db: Session) -> None:
@@ -32,3 +35,15 @@ class ArticleService:
     def get_all_articles(self, limit: int = 100, offset: int = 0) -> list[ArticleResponseSchema]:
         articles = self.article_repository.get_all(limit=limit, offset=offset)
         return [ArticleResponseSchema.model_validate(article) for article in articles]
+    
+    def update_article(self, article_id: str, update_data: ArticleUpdateSchema, current_user_id: str) -> ArticleResponseSchema:
+        article = self.article_repository.get_by_id(article_id=article_id)
+        if article is None:
+            raise ArticleNotFound("Article not found")
+        if article.author_id != current_user_id:
+            raise PermissionDenied("You are not the author of this article")
+        
+        updated_article = self.article_repository.update(article=article, title=update_data.title, content=update_data.content)
+
+        self.db.commit()
+        return ArticleResponseSchema.model_validate(updated_article)
